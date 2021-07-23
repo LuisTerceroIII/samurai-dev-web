9661d786ee83809a56ad3687ac67c4a688030590
Luis <luis.espinoza.nav@live.com>
2021-07-23 04:01:50 -0300

    test depoy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import Landing from "../sections/Landing/Landing";
 import Header from "../layouts/header/Header";
-import AboutMe from "../sections/AboutMe/AboutMe";
 //TODO: Compartir varibales css desde un archivo.
 const IndexPage = () : JSX.Element => {
   return (
