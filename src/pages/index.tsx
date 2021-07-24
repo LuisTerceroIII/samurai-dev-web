380818afed2c2bed450ef049e685a342a16fa2c8
Luis <luis.espinoza.nav@live.com>
2021-07-23 23:26:54 -0300

    test deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import * as React from "react"
 import Landing from "../sections/Landing/Landing";
 import Header from "../layouts/header/Header";
+import * as styles from "../assets/css/global.module.css";
 //TODO: Compartir varibales css desde un archivo.
 const IndexPage = () : JSX.Element => {
   return (
-      <>
+      <div className={styles.global}>
           <Header>
               <Landing/>
           </Header>
-      </>
+      </div>
   )
 }
 
